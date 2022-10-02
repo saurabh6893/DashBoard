5707acfb64b4b5fefd9efbe455ae13228181c2df
saurabh <saurabhkedar@gmail.com>
2022-10-02 17:46:26 +0530

    added cards: proceeding to capacitor

--- src/Pages/Display.tsx
+++ src/Pages/Display.tsx
@@ -3,16 +3,22 @@ import { AppContext } from '../App'
 
 const Display = () => {
   const { data }: any = useContext(AppContext)
+
   return (
-    <div>
-      <h1>{data ? data.firstname : 'No data Recieved'}</h1>
-      <h1>{data?.lastname}</h1>
-      <h1>{data?.email}</h1>
-      <h1>{data?.phone}</h1>
-      <h1>{data?.age}</h1>
-      <h1>{data?.country}</h1>
-    </div>
+    <>
+      {data ? <div className='card'>
+        <h1 className='cName'>Full Name: {data.firstname} {data.lastname}</h1>
+        <div className="contacx">
+          <h1 className='cEmail'>Email at {data.email}</h1>
+          <h1 className='cNum'>Phone Number {data.phone}</h1>
+        </div>
+        <h1 className='cAge'>Age :{data.age}</h1>
+        <h1 className='cCountry'>Country: {data.country}</h1>
+      </div> : <div className='card'>
+        <h1>Data not available</h1></div>}
+    </>
   )
+
 }
 
 export default Display
