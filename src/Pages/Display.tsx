8bcb7acbac044156be431a2806b74b984d87411d
saurabh <saurabhkedar@gmail.com>
2022-10-02 17:07:54 +0530

    added type fix for the Display

--- src/Pages/Display.tsx
+++ src/Pages/Display.tsx
@@ -5,12 +5,12 @@ const Display = () => {
   const { data }: any = useContext(AppContext)
   return (
     <div>
-      <h1>{data.firstname}</h1>
-      <h1>{data.lastname}</h1>
-      <h1>{data.email}</h1>
-      <h1>{data.phone}</h1>
-      <h1>{data.age}</h1>
-      <h1>{data.country}</h1>
+      <h1>{data ? data.firstname : 'No data Recieved'}</h1>
+      <h1>{data?.lastname}</h1>
+      <h1>{data?.email}</h1>
+      <h1>{data?.phone}</h1>
+      <h1>{data?.age}</h1>
+      <h1>{data?.country}</h1>
     </div>
   )
 }
